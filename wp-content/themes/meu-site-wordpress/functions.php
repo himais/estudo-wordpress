09c6b1a80ae82b5c7d867024356e9602acdcfae1
himais <maira.tabata@multiedro.com.br>
2017-06-28 10:58:12 -0300

    Alteração no css inicial e inclusão das referências no arquivo functions.php

--- wp-content/themes/meu-site-wordpress/functions.php
+++ wp-content/themes/meu-site-wordpress/functions.php
@@ -107,6 +107,14 @@ add_action( 'widgets_init', 'meu_site_wordpress_widgets_init' );
 function meu_site_wordpress_scripts() {
 	wp_enqueue_style( 'meu-site-wordpress-style', get_stylesheet_uri() );
 
+	//wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600'); 
+
+	wp_enqueue_style('materialize-css', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css');
+
+	wp_enqueue_script('materialize-js', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js');  
+
+	wp_enqueue_script('font-awesome', 'https://use.fontawesome.com/01a4bfecfa.js');
+
 	wp_enqueue_script( 'meu-site-wordpress-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'meu-site-wordpress-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
